4522b151b22e9b4860e7e2ce34441c208647a81e
Oscar Riva <oscar.riva@bsmart.it>
2019-06-06 15:20:15 +0200

    finally got form working

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,21 +1,25 @@
 class SubmissionsController < ApplicationController
+  def initialize
+    @games = Game.showcase
+    @tags = ActsAsTaggableOn::Tag.showcase
+    super
+  end
+
   def create
     @submission = Submission.new(submission_params)
 
-    respond_to do |format|
-      if @submission.save
-        flash[:notice] = 'Your shot is on its way.'
-        format.html  { redirect_to(:controller => "pages", :action => "submit") }
-      else
-        format.html  { render "pages/submit" }
-      end
+    if @submission.save
+      flash[:success] = "Your shot is on its way."
+      redirect_to controller: 'pages', action: 'submit'
+    else
+      flash[:error] = "Uh oh, something went wrong. Try again."
+      redirect_to controller: 'pages', action: 'submit'
     end
   end
 
   private
 
   def submission_params
-    params.require(:email)
     params.permit(:email, :image, :game)
   end
 end
