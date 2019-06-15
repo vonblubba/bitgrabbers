081f69016ef74f068e548c1a990010765c32b13f
vonblubba <oscar.ruva@gmail.com>
2019-06-15 23:09:55 +0000

    published creenshot bug

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -1,11 +1,11 @@
 class ScreenshotsController < ApplicationController
   def index
-    @screenshots = Screenshot.order(publication_date: :desc)
+    @screenshots = Screenshot.published.order(publication_date: :desc)
     #@screenshots = Screenshot.order(publication_date: :desc).paginate(page: params[:page], per_page: 12)
   end
 
   def show
-    @screenshot = Screenshot.friendly.find(params[:id])
+    @screenshot = Screenshot.published.friendly.find(params[:id])
     @page_title = @screenshot.title
   end
 end
