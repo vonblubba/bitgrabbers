adf0d72a9ae1bf18df20916f508e890082b5d911
vonblubba <oscar.ruva@gmail.com>
2019-06-29 15:46:24 +0000

    attempting some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'robots', to: 'pages#robots', as: 'robots'
   get 'rss', to: 'rss#index', as: 'rss_feed'
 
-  #devise_for :users, ActiveAdmin::Devise.config
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, ActiveAdmin::Devise.config
+  #devise_for :users, ActiveAdmin::Devise.config, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   #devise_scope :user do
   #  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
