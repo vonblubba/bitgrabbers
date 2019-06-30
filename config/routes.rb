db3a695bb48f099182edb67ccd28119f1a8b5f2c
vonblubba <oscar.riva@gmail.com>
2019-06-30 10:00:08 +0200

    facebook login ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,17 @@ Rails.application.routes.draw do
   get 'robots', to: 'pages#robots', as: 'robots'
   get 'rss', to: 'rss#index', as: 'rss_feed'
 
-  devise_for :users, ActiveAdmin::Devise.config
-  #devise_for :users, ActiveAdmin::Devise.config, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  #devise_for :users, ActiveAdmin::Devise.config
+  #devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   #devise_scope :user do
   #  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
   #  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   #end
+
+  devise_config = ActiveAdmin::Devise.config
+  devise_config[:controllers][:omniauth_callbacks] = 'users/omniauth_callbacks'
+  devise_for :users, devise_config
 
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
