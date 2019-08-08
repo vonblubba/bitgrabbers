5b782246785703eb3359a1049db53fa0abdfc3a2
Oscar Riva <oscar.riva@bsmart.it>
2019-08-08 10:30:18 +0200

    bugfix: wrong oubdate when editing

--- app/admin/screenshots.rb
+++ app/admin/screenshots.rb
@@ -19,7 +19,7 @@ ActiveAdmin.register Screenshot do
   filter :game_id
 
   form(:html => { :multipart => true }) do |f|
-    f.object.publication_date = Screenshot.farthest_publication_date
+    f.object.publication_date ||= Screenshot.farthest_publication_date
     f.object.user = current_user
     f.inputs do
       user_collection = User.all.map { |user| ["#{user.email}", user.id ]}
